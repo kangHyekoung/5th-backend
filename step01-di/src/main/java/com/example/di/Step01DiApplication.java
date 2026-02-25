38a597a272cd8edb102a6b5c7388856afbbbdd8a
강혜경 <kanghyekoung3694@gmail.com>
2026-02-25 17:26:02 +0900

    source

---
+++ step01-di/src/main/java/com/example/di/Step01DiApplication.java
@@ -0,0 +1,13 @@
+package com.example.di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Step01DiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Step01DiApplication.class, args);
+	}
+
+}
